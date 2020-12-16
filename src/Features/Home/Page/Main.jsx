922740ec0fbcf5b7e6637e9d3b3ae96f86477119
thanghuy <huythang0903@gmail.com>
2020-12-16 10:27:28 +0700

    cap nhap lan 3

--- src/Features/Home/Page/Main.jsx
+++ src/Features/Home/Page/Main.jsx
@@ -1,11 +1,28 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
+import ProductAPI from '../../../API/ProductApi';
 import Banner from '../Component/Banner';
-import ListProduct from '../Component/Newproduct';
+import ListProduct from '../../Shop/Component/ListProduct';
 const Main = () => {
+    const [listProduct,setListProduct] = useState([]);
+
+    useEffect(()=>{
+        const fetchProudct = async () =>{
+            try {
+                var params = {
+                    orderBy : "DEC"
+                }
+                const resp = await ProductAPI.getProduct(params);
+                setListProduct(resp.data);
+            } catch (error) {
+                
+            }
+        }
+        fetchProudct();
+    },[])
     return (
         <Fragment>
             <Banner />
-            <ListProduct />
+            <ListProduct productList={listProduct} />
         </Fragment>
     );
 };
